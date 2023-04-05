615ba0e1ef23813760d39c0394d41a92511ead79
VINOTH KANNA J <113609815+vinoth2407@users.noreply.github.com>
2023-04-05 10:22:48 +0530

    Calc Project
    Calc Project

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React,{StrictMode} from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Clock from './Yoga/25+clock/25+';
+//import Calc from './Yoga/calc/calc2';
+//import App from './Yoga/markdown/mark';
+//import App from './Yoga/drum/Quote/Quote';
+//import Yoga from './Yoga/drum/drum';
+//import App1 from './Yoga/calc1';
+//import App from './Yoga/calc/calc';
+//import Check from './components/checkStatus';
+//import 'bootstrap/dist/css/bootstrap.css';
+
+//import ValidationTextFields from './test727721euit179/form';
+//import DropMenu from './test727721euit179/dropMenu';
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <Clock/>
+  </StrictMode>
+);
+
+
